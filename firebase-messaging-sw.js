e09018b69e6abc8a011e1082e8f4fd9c4819a09a
Ivan Medvedev <chuchello@gmail.com>
2019-02-15 02:04:29 +0300

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,22 @@
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/5.8.3/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.8.3/firebase-messaging.js');
 
 firebase.initializeApp({
     messagingSenderId: '525340006745' // troque pelo seu sender id 
 });
 
 const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function (payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    const notification = JSON.parse(payload.data.notification);
+    // Customize notification here
+    const notificationTitle = notification.title;
+    const notificationOptions = {
+        body: notification.body,
+        icon: notification.icon
+    };
+
+    return self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
